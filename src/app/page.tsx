bc545140f6150b6e966e0c0b8ca5bd3ee7036982
Jini's Macbook <jinismacbook@Jiniss-MacBook-Air.local>
2025-06-10 23:30:13 -0700

    Home page design completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
+import TechMarquee from "@/components/TechMarquee";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col lg:flex-col items-center w-full px-6">
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-between w-full gap-10 py-16 px-14">
+        <div className="max-w-xl w-full">
+          <h1 className="text-6xl font-bold text-peach-900 mb-2">Hi, I’m Jinita Patel</h1>
+          <h2 className="text-2xl text-peach-500 font-semibold mb-8">Full Stack Developer</h2>
+          <p className="text-peach-700 text-xl mb-10">
+            5+ years building scalable web apps with React, Node, and Java. I write clean code,
+            scale systems, and mentor junior devs. I’m passionate about creating
+            user-friendly experiences and solving complex problems with elegant solutions.
+          </p>
+          <div className="flex gap-10">
+            <Link href="/blog" className="px-5 py-2 border border-peach-600 bg-peach-400 text-white rounded-md shadow-md hover:bg-peach-50 hover:text-peach-600">Read My Blog</Link>
+            <Link href="/experience" className="px-5 py-2 border border-peach-600 text-peach-600 rounded-md hover:bg-peach-400 hover:text-white">View Experience</Link>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="w-100 h-100 relative rounded-full overflow-hidden border-4 border-peach-200 shadow-md">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/profile-pic.png"
+            alt="Jinita Patel"
+            className="object-cover w-full h-full"
+            fill
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
+      <div className="w-full mb-10" />
+      <TechMarquee />
     </div>
   );
 }
